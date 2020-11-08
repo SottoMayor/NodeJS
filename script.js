7471f93f0d35a51e2ef8451857aef78ef7b06cf8
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-08 11:16:44 -0400

    What is JavaScript?

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+
+What's JavaScript?
+
+'JavaScript is a structured interpreted programming language,
+ high-level scripting with weak dynamic typing and multiparadigm. 
+ Along with HTML and CSS, JavaScript is one of the three main 
+ technologies on the World Wide Web.
+ It is currently the main language for client-side programming 
+ in web browsers. It is also widely used on the server side 
+ through environments such as node.js.
+ '
+
+*/
