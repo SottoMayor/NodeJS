0925608a748a20ac5df76ae15168fc5d68d7fd9f
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-10 15:19:32 -0400

    functions closure

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 /*
 
-What is Nan...
-
-NaN is Not a Number.
-
-
-isNaN(element) - how to know if an element is Not a Number (NaN)
+Functions Closures 
 
 */
 
 "use strict";
 
-var string = 'abc';
-var number = 4;
+function hello(name) {
 
-var nan = string/number
+    var text = 'hello ' + name;
+    return function () {
+        console.log(text);
+    }
 
-console.log(nan); //NaN
+};
 
-console.log(NaN == 1); //false
+var helloDavid = hello('David');
 
-console.log(isNaN(nan));
+helloDavid();
+
+
