a1c14f453f8c97256670ac2c87f14e0f41fc2757
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-13 11:38:59 -0400

    routing requests

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
 
-var server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
 
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
+    //routing requests
+    const url = req.url;
+    if (url == '/'){
 
-    //Sending responses...
-    res.setHeader('Content-Type', 'text/html'); //The first argument is the pattern of server and the secondy is our response.
+        res.setHeader('Content-Type', 'text/html'); 
+        res.write('<html>');
+        res.write('<head><title>My Node Page</title></head>');
+        res.write('<body><form method="POST" action="/message"><input type="text"><button type="submit">Send!</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    };
+
+    res.setHeader('Content-Type', 'text/html'); 
     res.write('<html>');
     res.write('<head><title>My Node Page</title></head>');
     res.write('<body><h1>Hello world!</h1></body>');
