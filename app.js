b1f42c859b9fbee07a85aae216be117d55766efc
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-13 18:22:30 -0400

    Parsing Requests Bodies

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const http = require('http');
+const fs = require('fs'); // file system
 
 const server = http.createServer((req, res) => {
 
-    //routing requests
     const url = req.url;
-    if (url == '/'){
+    if (url == '/') {
 
-        res.setHeader('Content-Type', 'text/html'); 
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>My Node Page</title></head>');
-        res.write('<body><form method="POST" action="/message"><input type="text"><button type="submit">Send!</button></form></body>');
+        res.write('<body><form method="POST" action="/message"><input type="text" name="message"><button type="submit">Send!</button></form></body>');
         res.write('</html>');
         return res.end();
     };
 
-    res.setHeader('Content-Type', 'text/html'); 
+    //Parsing Requests Bodies
+    var method = req.method;
+    if (url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', chunck => {
+            //console.log(chunck)
+            body.push(chunck);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            //console.log(parsedBody);
+            var message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My Node Page</title></head>');
     res.write('<body><h1>Hello world!</h1></body>');
