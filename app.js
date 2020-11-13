f6334135a847ab5ec35577d89d3d04c52bd5bfa3
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-13 11:01:07 -0400

    sending responses

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const http = require('http');
 
 var server = http.createServer((req, res) => {
 
-    //Basic of Requests...
     console.log(req.url, req.method, req.headers);
     //process.exit();
 
+    //Sending responses...
+    res.setHeader('Content-Type', 'text/html'); //The first argument is the pattern of server and the secondy is our response.
+    res.write('<html>');
+    res.write('<head><title>My Node Page</title></head>');
+    res.write('<body><h1>Hello world!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
