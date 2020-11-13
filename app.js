ed0178f380cdf85b106c4a624b401f37c1af8559
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-13 10:33:45 -0400

    Node lifecycle and event loop

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require('http');
 var server = http.createServer((req, res) => {
 
     console.log(req);
+    //process.exit(); -> If we wanna stop the NodeJs loop of the serve, we execute process.exit(); 
 
 });
 
