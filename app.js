138609fedc74a1225f6d533c1df1e30b16b90aad
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-13 10:42:42 -0400

    Basic of requests

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const http = require('http');
 
 var server = http.createServer((req, res) => {
 
-    console.log(req);
-    //process.exit(); -> If we wanna stop the NodeJs loop of the serve, we execute process.exit(); 
+    //Basic of Requests...
+    console.log(req.url, req.method, req.headers);
+    //process.exit();
 
 });
 
 server.listen(3000);
-//Exec in your nagivation 'localhost:3000' if you use localhost and the port is 3000
