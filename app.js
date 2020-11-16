7fd843a06ffbbfd982d8f390be2b7e236336d093
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-16 09:12:55 -0400

    sending a response and rewriting code with expressJS tools

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const http = require('http');
+//rewriting code with expressJS tools
+
+/*const http = require('http');*/ //we don't need anymore!
 
 const express = require('express');
 const app = express();
-
-//Adding middleware...
 
 app.use((req, res, next) => {
 
     console.log('I\'m a middleware!');
     
-    next(); //Allows the request to continue to the next middleware in line!
+    next();
 });
 
 app.use((req, res, next) => {
 
     console.log('I\'m a another middleware! ihullll!');
+    res.send('<h2>Hello World!!!</h2>')
 
 });
 
+//we don't need anymore!
+/*
 const server = http.createServer(app);
 
 server.listen(3000);
+*/
+
+app.listen(3000);
