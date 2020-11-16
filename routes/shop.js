9361f59cd297b118e8f864b26368d322453f1ebd
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-16 15:01:57 -0400

    using express router

---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req, res, next) => {
+
+    res.send('<h2>Hi... HOME page!</h2>');
+
+});
+
+module.exports = router;
