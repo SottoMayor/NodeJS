a9e4346e57445373e9d5bc9b8a108ffcf462653a
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-16 09:47:28 -0400

    Retaking...

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 const express = require('express');
 const app = express();
 
-//Handling Different Routes
+/*
+app.use((req, res, next) => {
 
-app.use('/', (req, res, next) =>{
-
-    console.log('It always will run!');
+    console.log('Hello World... I am the first middleware!');
 
     next();
+});
+
+app.use((req, res, next) => {
+
+    console.log('Hello World... I am the last middleware!');
+
+    res.send('<h2>I am the Boss!</h2>');
+});
+*/
+
+app.use('/users',(req, res, next) => {
+
+    console.log('Users page!')
+    res.send('<h2>Hello... USERS page!</h2>');
 
 });
 
-app.use('/add-product', (req, res, next) => {
+app.use('/',(req, res, next) => {
 
-    res.send('<h2>The ADD PRODUCT page!</h2>');
-    console.log('ADD PRODUCT page!');
-
-});
-
-app.use('/', (req, res, next) => {
-
-    res.send('<h2>The HOME page!</h2>');
-    console.log('HOME page');
+    console.log('HOME page!')
+    res.send('<h2>Hi... HOME page!</h2>');
 
 });
+
 
 app.listen(3000);
