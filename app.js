2c2bbd518a4a213d9b8a69b6871d05fdf2aa48da
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-16 08:58:53 -0400

    Adding middlewares

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+//Adding middleware...
+
+app.use((req, res, next) => {
+
+    console.log('I\'m a middleware!');
+    
+    next(); //Allows the request to continue to the next middleware in line!
+});
+
+app.use((req, res, next) => {
+
+    console.log('I\'m a another middleware! ihullll!');
+
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
