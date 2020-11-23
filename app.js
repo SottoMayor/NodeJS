82c8722ea11e8de763941d8bfef1376200c262a4
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-23 11:27:43 -0400

    static files(adding css files to the project).

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const shopRoutes = require('./routes/shop');
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res, next) => {
 
     res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
