4a11354dc36ed86a90e5ecf928cd97badea4d101
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-02 11:54:04 -0400

    Pre-Populating the edit product page with data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,31 +1,39 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/edit-product', {
-      docTitle: 'Add Product',
-      path: '/admin/add-product'
-    });
+  res.render('admin/edit-product', {
+    docTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  });
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const description = req.body.description;
-    const price = req.body.price;
-    const product = new Product(title, imageUrl, description, price);
-    product.save();
-    res.redirect('/');
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit
-  if (!editMode){
-    res.redirect('/')
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
   }
-  res.render('admin/edit-product', {
-    docTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      docTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
   });
 };
 
