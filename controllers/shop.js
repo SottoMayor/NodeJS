b7408cd7bb8c445466a25f5d92d690ebe1f82220
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-06 13:07:42 -0400

    displaying cart items in cart page

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,8 +33,20 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', { docTitle: 'Cart Page', path: '/cart' })
-}
+  Cart.getCart( cart => {
+    Product.fetchAll(products =>{
+      const cartProducts = [];
+      for(product of products){
+        const cartProductData = cart.products.find(prod => prod.id ===  product.id);
+        if(cartProductData){
+          cartProducts.push({productData: product, qty: cartProductData.qty })
+        }
+      }
+      res.render('shop/cart', { docTitle: 'Cart Page', path: '/cart', products: cartProducts});
+    });
+  });
+};
+  
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
