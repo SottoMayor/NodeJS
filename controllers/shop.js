f43f91ed195d1ecfec1f614cb2b3f098831a9d32
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-11 14:06:29 -0400

    Retrieving and Finding products

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,12 +1,12 @@
-const { findById } = require('../models/product');
 const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then( ([rows, fieldData]) => {
+
+  Product.findAll()
+  .then( products => {
     res.render('shop/index', {
-      prods: rows,
+      prods: products,
       docTitle: 'Shop',
       path: '/'
     })
@@ -18,11 +18,10 @@ exports.getIndex = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
 
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then( ([product]) => {
-    // product is an array still! So we must set product[0].
+  Product.findByPk(prodId)
+  .then( product => {
     res.render('shop/product-detail', 
-    { product: product[0], 
+    { product: product, 
       docTitle: 'Details about ' + product.title, 
       path:'/products' 
     })
@@ -33,10 +32,10 @@ exports.getProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
 
-  Product.fetchAll()
-  .then( ( [rows, fieldData] ) => {
+  Product.findAll()
+  .then(products => {
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       docTitle: 'Shop',
       path: '/products'
     })
@@ -44,7 +43,6 @@ exports.getProducts = (req, res, next) => {
   .catch(
     err => console.log(err)
   );
-
 }
 
 exports.getCart = (req, res, next) => {
