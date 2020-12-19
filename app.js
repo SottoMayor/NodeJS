eb22de4dea1c155aa04075cb805e038e54142192
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-19 15:41:33 -0400

    adding and using a user model

--- app.js
+++ app.js
@@ -11,23 +11,23 @@ const shopRoutes = require('./routes/shop');
 
 const mongoose = require('mongoose');
 
-//const User = require('./models/user');
+const User = require('./models/user');
 
 app.set('view engine','ejs');
 app.set('views', 'views');
 
-/* 
+
 app.use((req, res, next) =>  {
-    User.findById('5fd9ff7fdd7a18b0375f4b46')
+    User.findById('5fde5726e01b330b60a66049')
     .then( user => { 
-    req.user = new User(user.name, user.email, user.cart, user._id);
+    req.user = user;
     next();
     })
     .catch( err => {
         console.log(err);
     })
 })
-*/
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -39,6 +39,20 @@ app.use(pageNotFound.pageNotFound);
 
 mongoose.connect('mongodb+srv://david:davidMongoDB@cluster0.bhiv4.mongodb.net/shop?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
 .then(() => {
+
+    User.findOne().then( user => {
+        if (!user){
+            const user = new User({
+                name: 'David',
+                email: 'david@teste.com',
+                cart: {
+                    items: []
+                }
+            });
+            user.save();
+        }
+    })
+
     app.listen(3000)
 })
 .catch((err) => {
