0bed8931245fd5213f9c9d52833b18d13c7a881c
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-19 19:05:30 -0400

    loading the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -47,8 +47,10 @@ exports.getProducts = (req, res, next) => {
 exports.getCart = (req, res, next) => {
 
   req.user
-  .getCart()
-  .then(products => {
+  .populate('cart.items.productId')
+  .execPopulate()
+  .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', { docTitle: 'Cart Page', path: '/cart', products: products});
     })
   .catch(err => {console.log(err)});
