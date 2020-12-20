1cf5b397747adc46b85e1f6651fb8e81937c754d
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-20 11:24:01 -0400

    initializing with cookies and sessions - adding a login form

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 const mongoose = require('mongoose');
 
@@ -31,6 +32,7 @@ app.use((req, res, next) =>  {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
