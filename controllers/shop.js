aa6c209408e750c48cf86176ce1f97690aa5b746
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-20 09:46:58 -0400

    creating and getting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getIndex = (req, res, next) => {
 
@@ -82,7 +83,21 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-  .addOrder()
+  .populate('cart.items.productId')
+  .execPopulate()
+  .then(user => {
+      const products = user.cart.items.map( i => {
+        return {quantity: i.quantity, product: i.productId}
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      })
+      return order.save()
+  })
   .then( () => {
     res.redirect('/orders');
   })
