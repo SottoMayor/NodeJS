50c6db8c8a6abebfbc48798665eb46dc585afe5a
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-23 11:26:54 -0400

    our update project status

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,3 +31,11 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   })
 }
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', { docTitle: 'Signup', path: '/signup', isAuthenticated: false })
+}
+
+exports.postSignup = (req, res, next) => {
+  res.redirect('/');
+}
