c06b4849bb74034fbe3d0ac8bd54aff369ab4062
David Sotto Mayor <ddsmmf@gmail.com>
2021-01-07 10:41:40 -0400

    validating product editing

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const path = require('path');
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-Auth');
+const {body} = require('express-validator')
 
 
-router.get('/add-product', isAuth, adminController.getAddProduct);
+router.get('/add-product', [
+    body('title').isAlphanumeric().isLength({min: 3}).trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({min: 3, max: 400}).trim()
+], isAuth, adminController.getAddProduct);
 
 router.get('/products', isAuth, adminController.getProducts);
 
@@ -13,7 +19,12 @@ router.post('/add-product', isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product', [
+    body('title').isString().isLength({min: 3}).trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({min: 3, max: 400}).trim()
+], isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
