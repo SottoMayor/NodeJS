1993673d668b9bb5dd9902f731620c5b5610c92c
David Sotto Mayor <ddsmmf@gmail.com>
2021-01-08 18:04:54 -0400

    handling multipart form data

--- routes/admin.js
+++ routes/admin.js
@@ -6,22 +6,20 @@ const isAuth = require('../middleware/is-Auth');
 const {body} = require('express-validator')
 
 
-router.get('/add-product', [
-    body('title').isString().isLength({min: 3}).trim(),
-    body('imageUrl').isURL(),
-    body('price').isFloat(),
-    body('description').isLength({min: 3, max: 400}).trim()
-], isAuth, adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
 router.get('/products', isAuth, adminController.getProducts);
 
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product', [
+    body('title').isString().isLength({min: 3}).trim(),
+    body('price').isFloat(),
+    body('description').isLength({min: 3, max: 400}).trim()
+], isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
 router.post('/edit-product', [
     body('title').isString().isLength({min: 3}).trim(),
-    body('imageUrl').isURL(),
     body('price').isFloat(),
     body('description').isLength({min: 3, max: 400}).trim()
 ], isAuth, adminController.postEditProduct);
