ad5ca2c3fede92e04e128faf47b6d041757771d6
David Sotto Mayor <ddsmmf@gmail.com>
2021-01-08 18:54:25 -0400

    filtering files by mimetype

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ const fileStorage = multer.diskStorage({
     },
 });
 
-app.use(multer({storage: fileStorage}).single('image'));
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' ){
+        cb(null, true);
+    }else{
+        cb(null, false);
+    }
+}
+
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
