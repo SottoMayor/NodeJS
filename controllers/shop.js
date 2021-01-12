51f9b065cf990090592a8a6256177ebbc790f982
David Sotto Mayor <ddsmmf@gmail.com>
2021-01-12 19:46:03 -0400

    reusing the pagination Logic and Controls

--- controllers/shop.js
+++ controllers/shop.js
@@ -62,12 +62,30 @@ exports.getProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
 
+  const page = +req.query.page || 1;
+  let totalItems;
+
+
+
   Product.find()
-  .then(products => { 
+  .countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  })
+  .then( products => {
     res.render('shop/product-list', {
       prods: products,
-      docTitle: 'Shop',
-      path: '/products'
+      docTitle: 'Products',
+      path: '/products',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     })
   })
   .catch( err => {
