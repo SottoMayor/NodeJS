fc284cc07c37b4ecae0720c03b4d9106a14952d2
David Sotto Mayor <ddsmmf@gmail.com>
2021-01-13 11:11:15 -0400

    manipulating the DOM

--- controllers/admin.js
+++ controllers/admin.js
@@ -177,8 +177,8 @@ exports.getProducts = (req, res, next) => {
 
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
 
   Product.findById(prodId)
   .then(product => {
@@ -192,11 +192,9 @@ exports.postDeleteProduct = (req, res, next) => {
   })
   .then( () => {
     console.log('Product Deleted');
-    res.redirect('/admin/products');
+    res.status(200).json({message: 'Success!'});
   })
   .catch( err => {
-    const error = new Error(err);
-    err.httpStatusCode = 500;
-    return next(error);
+    res.status(500).json({message: 'Deleting product failed!'});
   } );
 }
--- routes/admin.js
+++ routes/admin.js
@@ -24,9 +24,7 @@ router.post('/edit-product', [
     body('description').isLength({min: 3, max: 400}).trim()
 ], isAuth, adminController.postEditProduct);
 
-router.post('/delete-product', isAuth, adminController.postDeleteProduct);
-
-
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 
 module.exports = router;
